cb81faf7dea930213bbb841d40a4b70484b2b3c8
chefberke <berkekanber@gmail.com>
2025-07-21 17:37:24 +0300

    Add Clarity script to layout for enhanced user tracking and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from "next/font/google";
 import "@/styles/globals.css";
 import SessionProvider from "@/components/providers/SessionProvider";
 import { QueryProvider } from "@/components/providers/QueryProvider";
+import Script from "next/script";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -25,6 +26,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="clarity-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "sif0uh70xw");
+            `,
+          }}
+        />
+      </head>
       <body className={`${poppins.variable} antialiased`}>
         <SessionProvider>
           <QueryProvider>{children}</QueryProvider>
