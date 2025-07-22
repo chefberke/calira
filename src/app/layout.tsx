a33875ef6570aab635e4dccf87251f55096b5c31
chefberke <berkekanber@gmail.com>
2025-07-22 23:01:49 +0300

    Remove Clarity script and replace with Databuddy script for enhanced user tracking and analytics.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,17 +28,11 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <Script
-          id="clarity-script"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "sif0uh70xw");
-            `,
-          }}
+          src="https://cdn.databuddy.cc/databuddy.js"
+          data-client-id="7XY11uPxCW85k7nOFdBEl"
+          data-enable-batching="true"
+          crossOrigin="anonymous"
+          async
         />
       </head>
       <body className={`${poppins.variable} antialiased`}>
